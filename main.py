145c663c20b93f395b4dd3c82ee0690ad7bef6aa
yuxiaojie0322-maker <yuxiaojie0322@gmail.com>
2026-08-28 16:47:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 
 # 配置区域
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=5cd426a6-8e7c-41ee-948c-c81c430619a4",
     # 添加更多链接
 ]
 
